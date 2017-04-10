3175b22f9a42195924a643c82c6431a201282939
Valentín Costa <valentincostam@gmail.com>
2017-04-10 00:12:03 -0300

    LCD added.

--- paddle-strokes-trainer.ino
+++ paddle-strokes-trainer.ino
@@ -1,9 +1,17 @@
+// Librerías
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
+#include <Fonts/FreeSansBold9pt7b.h>
+#include <Fonts/FreeSansBold24pt7b.h>
+
 // Pines
 const int PULSADOR_SUBIR = 2; // Pulsador que aumenta el número de paladas por minuto.
 const int PULSADOR_BAJAR = 3; // Pulsador que disminuye el número de paladas por minuto.
 const int LED_VERDE      = 4; // LED de color azul.
 const int LED_ROJO       = 5; // LED de color verde.
 const int BUZZER         = 6; // Buzzer.
+// const int LCD_BACKLIGHT  = 9; // Backlight del LCD.
 
 // Constantes
 const int MAXIMO_PALADAS    = 150; // Número máximo de paladas por minuto.
@@ -21,39 +29,81 @@ boolean buzzer_on              = false;     // Indica si el buzzer está prendid
 int led_on                     = LED_ROJO;  // Indica qué LED está prendido.
 int led_off                    = LED_VERDE; // Indica qué LED está apagado.
 
+// LCD
+// pin 13 - Serial clock out (SCLK)
+// pin 11 - Serial data out (DIN)
+// pin 8  - Data/Command select (D/C)
+// pin 12 - LCD chip select (CS)
+// pin 10 - LCD reset (RST)
+Adafruit_PCD8544 lcd = Adafruit_PCD8544(13, 11, 8, 12, 10);
+
 
 /*
- * Inicializa los pines, el serial y el display.
+ * Inicializa los pines, el serial y el lcd.
  */
 void setup() {
-  // Define pines de entrada
+  // Define pines de entrada y salida
   pinMode(PULSADOR_SUBIR, INPUT);
   pinMode(PULSADOR_BAJAR, INPUT);
-
-  // Define pines de salida
-  pinMode(LED_VERDE, OUTPUT);
-  pinMode(LED_ROJO,  OUTPUT);
-  pinMode(BUZZER,    OUTPUT);
+  // pinMode(LCD_BACKLIGHT,  OUTPUT);
+  pinMode(LED_VERDE,      OUTPUT);
+  pinMode(LED_ROJO,       OUTPUT);
+  pinMode(BUZZER,         OUTPUT);
   
   // Inicializa los LEDs
   digitalWrite(LED_VERDE, LOW);
   digitalWrite(LED_ROJO,  HIGH);
+
+  // Enciende el backlight del LCD
+  // analogWrite(LCD_BACKLIGHT, 120);
     
   // Inicializa el serial
   Serial.begin(9600);
   Serial.println("Serial andando!");
+
+  // Inicializa el lcd
+  lcd.begin();
+  lcd.setContrast(50); // Establece el contraste del LCD.
+  lcd.clearDisplay();  // Limpia la pantalla y el buffer.
+
+  // Muestra el mensaje de bienvenida
+  lcd.setCursor(0,12);
+  lcd.setFont(&FreeSansBold9pt7b);
+  lcd.println("Hola");
+  lcd.println("mostro!");
+  lcd.display();
+  delay(3000);
+  lcd.clearDisplay();
+
+  // Muestra el ritmo inicial
+  mostrarPPM();
 } // Fin de setup()
 
 
 /*
- * Actualiza el intervalo de tiempo que debe haber entre las paladas y el
- * número de paladas por minuto en el display.
+ * Muestra en el lcd y el serial la cantidad de paladas por minuto.
+ */
+void mostrarPPM() {
+  lcd.clearDisplay();
+  lcd.setCursor(0,7);
+  lcd.setFont();
+  lcd.print("PPM");
+  lcd.setCursor(0,38);
+  lcd.setFont(&FreeSansBold24pt7b);
+  lcd.println(paladas);
+  lcd.display();
+  
+  Serial.print("Paladas por minuto: ");
+  Serial.println(paladas, DEC);
+} // Fin de mostrarPPM()
+
+
+/*
+ * Actualiza el intervalo de tiempo que debe haber entre las paladas.
  */
 void actualizarRitmo() {
   intervalo = (long)(60 / (float)paladas * 1000);
-   
-  Serial.print("Paladas por minuto: ");
-  Serial.println(paladas, DEC);
+  mostrarPPM();
 } // Fin de actualizarRitmo()
 
 
@@ -159,7 +209,7 @@ void detectarPulsadores() {
     while (digitalRead(PULSADOR_SUBIR) == HIGH && digitalRead(PULSADOR_BAJAR) == HIGH)
       delay(100);
   }
-}
+} // Fin de detectarPulsadores()
 
 /* 
  * Bucle que se repite infinitamente. Enciende de forma intermitente los LEDs y
@@ -168,4 +218,4 @@ void detectarPulsadores() {
 void loop() {  
   destellar();
   detectarPulsadores();
-} // Fin de loop ()
+} // Fin de loop()
